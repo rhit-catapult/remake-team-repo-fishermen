44efe02b4ab1211c2b7e5c35ffed9f9043942127
brunoeastudillo-create <bruno.e.astudillo@gmail.com>
2026-06-27 09:52:23 -0400

    Update batting_system.py

--- batting_system.py
+++ batting_system.py
@@ -7,3 +7,22 @@ class BattingSystem:
     def draw(self):
         pygame.draw.rect(self.screen, (220,220,220), (50,475,120,120))
 
+class Bat:
+    def __init__(self, screen, (x=70, y=70), BatSystem.bat.png):
+        """ Creates a Cloud sprite that will produce Raindrop objects.  The cloud will be moving around. """
+        self.screen = screen 
+        self.x = x
+        self.y = y 
+        self.image = pygame.image.load(BatSystem.bat.png)
+
+
+
+class Bat:
+    def __init__(self, screen, x, y, BatSystem.bat.png ):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load(BatSystem.bat.png)
+
+    def draw(self):
+        self.screen.blit(self.image, (self.x, self.y))
