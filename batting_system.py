a0e68a0f276292c55c066156b7aadd5accbd6c05
brunoeastudillo-create <bruno.e.astudillo@gmail.com>
2026-06-29 11:49:22 -0400

    Added to bat.

--- batting_system.py
+++ batting_system.py
@@ -3,26 +3,25 @@ import pygame
 class BattingSystem:
     def __init__(self, screen: pygame.Surface):
         self.screen = screen
-
+        self.bat = Bat(screen, 110, 675)
     def draw(self):
         pygame.draw.rect(self.screen, (220,220,220), (50,475,120,120))
+        self.bat.move()
+        self.bat.draw()
 
 class Bat:
-    def __init__(self, screen, (x=70, y=70), BatSystem.bat.png):
-        """ Creates a Cloud sprite that will produce Raindrop objects.  The cloud will be moving around. """
-        self.screen = screen 
-        self.x = x
-        self.y = y 
-        self.image = pygame.image.load(BatSystem.bat.png)
-
-
-
-class Bat:
-    def __init__(self, screen, x, y, BatSystem.bat.png ):
+    def __init__(self, screen, x, y):
         self.screen = screen
         self.x = x
         self.y = y
-        self.image = pygame.image.load(BatSystem.bat.png)
-
+        self.speed = 4
+        self.image = pygame.image.load("BatSystem.bat.png")
+        self.image = pygame.transform.scale(self.image, (200, 200))
     def draw(self):
-        self.screen.blit(self.image, (self.x, self.y))
+        self.screen.blit(self.image, (self.x - 100, self.y - 100))    
+    def move(self):
+        self.x += self.speed
+        if self.x > (175):
+            self.speed = -self.speed
+        if self.x < (50):
+            self.speed = -self.speed
