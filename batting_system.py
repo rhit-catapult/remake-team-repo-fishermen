16c0a4cf784732669e02e89a237c921ef5ea8c97
brunoeastudillo-create <bruno.e.astudillo@gmail.com>
2026-06-29 12:13:31 -0400

    Images in.

--- batting_system.py
+++ batting_system.py
@@ -1,14 +1,17 @@
 import pygame
+import random
 
 class BattingSystem:
     def __init__(self, screen: pygame.Surface):
         self.screen = screen
         self.bat = Bat(screen, 110, 675)
+        self.ball = Ball(screen, 110, 600)
     def draw(self):
         pygame.draw.rect(self.screen, (220,220,220), (50,475,120,120))
         self.bat.move()
         self.bat.draw()
-
+        self.ball.draw()
+        self.ball.move()
 class Bat:
     def __init__(self, screen, x, y):
         self.screen = screen
@@ -25,3 +28,16 @@ class Bat:
             self.speed = -self.speed
         if self.x < (50):
             self.speed = -self.speed
+
+class Ball:
+    def __init__(self, screen, x, y):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("Baseball.png")
+        # self.image = pygame.transform.scale(self.image, (200, 200))
+    def draw(self):
+        self.screen.blit(self.image, (self.x - 8, self.y - 8))    
+    def move(self): 
+        self.x = random.randint(58, 162)
+        self.y = random.randint(483, 587)
